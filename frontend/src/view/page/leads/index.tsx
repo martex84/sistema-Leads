cf34eda2eacc441bb49f66caa53fa3544cd35d61
martex84 <martex849@gmail.com>
2021-10-31 03:40:43 -0300

    Inclusao Header, Button e Inicio do Componentes

--- frontend/src/view/page/leads/index.tsx
+++ frontend/src/view/page/leads/index.tsx
@@ -1,8 +1,40 @@
+import { Logo } from "../../components/logo";
 
+import "./styles.scss";
 
 function Leads() {
     return (
         <>
+            <div className="containerLeads">
+                <div>
+                    <div className="containerHeaderLeads">
+                        <Logo />
+                        <div className="detalhePainelLeads">
+                            Painel de Leads
+                        </div>
+                    </div>
+                    <div className="containerButtonNewLeads">
+                        <button>
+                            Novo Lead (+)
+                        </button>
+                    </div>
+                    <div className="containerConteudoLeads">
+                        <ul className="containerListaConteudoLeads">
+                            <li className="sumarioListaConteudoLeads">
+                                <div className="containerDescricaoConteudoLeads">
+                                    Cliente em Potencial
+                                </div>
+                                <div className="containerDescricaoConteudoLeads">
+                                    Dados Confirmados
+                                </div>
+                                <div className="containerDescricaoConteudoLeads">
+                                    Reunião Agendada
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
