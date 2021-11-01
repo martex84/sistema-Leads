a69e3d47b5acb3c185dd28389d5786426d428c99
martex84 <martex849@gmail.com>
2021-11-01 02:42:38 -0300

    Inclusao Layout New Leads

--- frontend/src/view/page/new-leads/index.tsx
+++ frontend/src/view/page/new-leads/index.tsx
@@ -5,7 +5,7 @@ import "./styles.scss";
 
 
 
-function Leads() {
+function NewLeads() {
 
     return (
         <>
@@ -14,11 +14,80 @@ function Leads() {
                     <div className="containerHeaderNewLeads">
                         <Logo />
                         <div className="detalhePainelNewLeads">
-                            Painel de Leads
+                            Novo Lead
                         </div>
                     </div>
                     <div className="containerConteudoNewLeads">
-
+                        <div className="containerInputNewLeads">
+                            <div>
+                                <span>Nome *</span>
+                                <input type="text" />
+                            </div>
+                            <div>
+                                <span>Telefone *</span>
+                                <input type="text" />
+                            </div>
+                            <div>
+                                <span>Email *</span>
+                                <input type="email" />
+                            </div>
+                        </div>
+                        <div className="containerAcoesNewLeads">
+                            <div className="tabelaOportunidadeNewLeads">
+                                <label>Oportunidades *</label>
+                                <ul className="listaOportunidadesNewLeads">
+                                    <li>
+                                        <div>
+                                            <input type="checkbox" />
+                                        </div>
+                                        <div className="containerInformacaoListaNewLeads"></div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <input type="checkbox" />
+                                        </div>
+                                        <div className="containerInformacaoListaNewLeads">
+                                            RPA
+                                        </div>
+                                    </li>
+                                    <li className="backgroundAlternativoListaNewLeads">
+                                        <div>
+                                            <input type="checkbox" />
+                                        </div>
+                                        <div className="containerInformacaoListaNewLeads">
+                                            Produto Digital
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <input type="checkbox" />
+                                        </div>
+                                        <div className="containerInformacaoListaNewLeads">
+                                            Analytics
+                                        </div>
+                                    </li>
+                                    <li className="backgroundAlternativoListaNewLeads">
+                                        <div>
+                                            <input type="checkbox" />
+                                        </div>
+                                        <div className="containerInformacaoListaNewLeads">
+                                            BPM
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <span />
+                                        </div>
+                                        <div className="containerInformacaoListaNewLeads" />
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="containerBotaoNewLeads">
+                                <button>
+                                    Salvar
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -26,4 +95,4 @@ function Leads() {
     );
 }
 
-export { Leads }
+export { NewLeads }
